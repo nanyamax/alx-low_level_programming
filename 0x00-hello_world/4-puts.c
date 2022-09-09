2e0edc2f26ff35c70b68f0dfde5ebfedec75a377
nanyamax <ifunanyasunday7@gmail.com>
2022-09-08 17:05:20 -0700

    Puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Takes no argument
+ *
+ * Description: printing a script to standard output
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
