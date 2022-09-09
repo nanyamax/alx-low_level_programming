93b6aedb4eabd5c26190d3f264de56236a9a9c2b
nanyamax <ifunanyasunday7@gmail.com>
2022-09-08 18:18:07 -0700

    Size of file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Takes no argument
+ *
+ * Description: prints the bytes for various data types to stdo
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
