73c414a82a54fd5ff1d7bb353152a4b32a424237
nanyamax <ifunanyasunday7@gmail.com>
2022-09-09 17:12:23 -0700

    Char numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print single numbers in base 10
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (1 <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
+
