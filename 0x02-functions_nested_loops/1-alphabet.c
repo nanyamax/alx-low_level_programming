ee945c2e871e075e67ee83bc495ecfd85c9fa8cb
nanyamax <ifunanyasunday7@gmail.com>
2022-09-13 15:06:38 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0 on success
+ */
+int main()
+{
+	for (c= ‘a’; c<= ‘z’; c++)
+
+		_putchar(c);
+		_putchar('\n');
+		return (0);
+}
+
