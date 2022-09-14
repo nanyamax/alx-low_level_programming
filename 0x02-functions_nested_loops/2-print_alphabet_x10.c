3bd444beff21ea0ea79a2a042f83831ee5ffd5bc
nanyamax <ifunanyasunday7@gmail.com>
2022-09-13 17:10:08 -0700

    10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10x
+ *
+ * Description: function to print lowercase alphabet 10 times
+ * Return: 0 on success
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a'
+		for (a = 'a'; a <= 'z'; a++)
+	int i;
+		for (i = a; i <= 10 ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+}
