054dffa1600721d2a140af62d0d6ed51faeab4f2
nanyamax <ifunanyasunday7@gmail.com>
2022-09-15 18:21:57 -0700

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks for uppercase
+ *
+ * c is a variable of the data type int
+ * Return: 1 if c is uppercase
+ * Return 0 otherwise
+ */
+int _isupper(int c)
+{
+	int isupper(int c);
+	int isupper_l(int c, locale_t locale);
+}
