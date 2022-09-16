c314448202aa5db214eef6a59e44d907ce3454d2
nanyamax <ifunanyasunday7@gmail.com>
2022-09-15 20:51:22 -0700

    1digits

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,15 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isupper(int);
+int _isdigit(int );
+int mul(int, int );
+int _putchar(char)
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
+void print_line(int i);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
-#endif // MAIN_H_INCLUDED
+endinif // MAIN
