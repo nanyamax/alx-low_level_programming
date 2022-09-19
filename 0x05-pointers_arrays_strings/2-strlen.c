bce3497374fa32109e1cfe5b463bd263e03f0cd4
nanyamax <ifunanyasunday7@gmail.com>
2022-09-19 15:12:30 -0700

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  *  _strlen - returns the length of a string
  *
@@ -7,7 +8,5 @@
  */
 int _strlen(char *s)
 {
-	int length;
-
-	length = strlen(*s);
+	size_t strlen(const char *s);
 }
