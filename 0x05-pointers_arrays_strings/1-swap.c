ff323ab28b01952fbed6a1c75239c916014bb518
nanyamax <ifunanyasunday7@gmail.com>
2022-09-19 13:04:20 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a: pointer
+ * @b: pointer
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
