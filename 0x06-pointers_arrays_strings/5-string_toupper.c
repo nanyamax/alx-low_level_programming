b7953186b7428b4500a353f3b3f663b1af3dc5a8
nanyamax <ifunanyasunday7@gmail.com>
2022-09-22 08:53:25 -0700

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
  * *string_toupper - changes lowercase to uppercase
  *
  * @str: string pointer
- * Return: corresponding uppercase of the changed lowercase 
+ * Return: corresponding uppercase of the changed lowercase
  */
 char *string_toupper(char *str)
 {
