d0ef9fa1a9fa703e51c1ab6a5b1e534d09bdd6b7
nanyamax <ifunanyasunday7@gmail.com>
2022-09-22 15:57:54 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -23,12 +23,12 @@ void print_number(int n)
 
 	while (d > 9)
 	{
-	 d /= 10;
-	 count *= 10;
+		d /= 10;
+		count *= 10;
 	}
 
-	for ( ; count >= 1; count /= 10)
+	for (; count >= 1; count /= 10)
 	{
-	 _putchar(((m / count) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
