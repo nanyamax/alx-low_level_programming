75ad6cf0c70aa437a1cfee2d64d8d949d29e81e7
nanyamax <ifunanyasunday7@gmail.com>
2022-09-22 05:31:55 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two string
+ * @s1: pointer to variable s1
+ * @s2: pointer to variable s2
+ * Return: 0,< 0, >0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	while (*(s1 + res) && *(s2 + res) && (*(s1 + res) == *(s2 + res)))
+		res++;
+	return (*(s1 + res) - *(s2 + res));
+}
