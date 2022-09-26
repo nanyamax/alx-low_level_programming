ea3431065abc90d19971f15e7f99948abb9faf8c
nanyamax <ifunanyasunday7@gmail.com>
2022-09-26 13:31:07 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,8 +14,13 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
+			if (j == 7)
+			{
 			_putchar(a[i][j]);
+			_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
 		}
-		_putchar('\n');
 	}
 }
