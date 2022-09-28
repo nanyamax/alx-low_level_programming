80c2aeb2333ec23efe619098ff61f05a5d6ef54d
nanyamax <ifunanyasunday7@gmail.com>
2022-09-28 11:58:21 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * helper - finds the square root of two numbers
+ *
+ * @i: the number
+ * @j: the number to the test for the square root of @i
+ * Return: square root
+ */
+int helper(int a, int b)
+{
+	if (j * j > i)
+		return (-1);
+	else if (j * j == a)
+		return (j);
+	else
+		return (helper(i, j + 1));
+
+	return (1);
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n:number
+ * Return: -1 if @n does not have a natural sqrt and sqrt of @n on success
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (helper(n, 1));
+}
