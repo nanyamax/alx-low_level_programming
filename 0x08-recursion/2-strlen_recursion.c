c9138e35fc4ac263d8d73e3ffef766bb2f1a6ef3
nanyamax <ifunanyasunday7@gmail.com>
2022-09-28 09:45:29 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string to be returned
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursions(s + 1));
+}
+
