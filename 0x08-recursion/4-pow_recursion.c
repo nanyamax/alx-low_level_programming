b92833c9d8c23a2a1d61d92d998da8f70f13d986
nanyamax <ifunanyasunday7@gmail.com>
2022-09-28 11:25:22 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ *
+ * @x: integer to be raised
+ * @y: integer power
+ * Return: -1 if y is lesser than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0 || y == 1)
+		return (1);
+
+		return (x * (_pow_recursion(x, y - 1)));
+}
+
