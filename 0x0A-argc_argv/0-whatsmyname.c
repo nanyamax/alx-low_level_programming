46ce0ff57071a593f0aa1791456ba294bb40a270
nanyamax <ifunanyasunday7@gmail.com>
2022-09-30 11:15:50 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,6 +9,6 @@
  */
 int main(int argc __attribute__((__unused__)), char *argv[])
 {
-	_printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int main(int argc __attribute__((__unused__)), char *argv[]);
 
 
-int _putchar(char)
+int _putchar(char);
+
 #endif
