827e3a50d981c2c38255ecce39c37c9fcbb45a03
nanyamax <ifunanyasunday7@gmail.com>
2022-09-30 16:50:28 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds positive integers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, count;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			if (atoi(argv[count]) > 0)
+			{
+				sum += atoi(argv[count]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+
+	return (0);
+}
