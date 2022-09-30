ba050015a5221da935eb255a1d6c694bc054ee88
nanyamax <ifunanyasunday7@gmail.com>
2022-09-30 11:27:52 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[] __attribute((__unused__)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
