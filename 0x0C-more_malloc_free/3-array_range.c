e9f6a686e30fc85218cd19dc2502978a5749e71b
nanyamax <ifunanyasunday7@gmail.com>
2022-10-06 12:06:33 -0700

    mandatory

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,9 +5,9 @@
  * array_range - creates an array of integers
  *
  * @min: smaller integer in the array
-   @max: largest integer in the array
- *  Return: pointer to the newly created array, NULL if min > max
- *  NULL if malloc fails
+ * @max: largest integer in the array
+ * Return: pointer to the newly created array, NULL if min > max
+ * NULL if malloc fails
  */
 int *array_range(int min, int max)
 {
