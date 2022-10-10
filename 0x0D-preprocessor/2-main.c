6df9c9c868de7b09cce5047ef391e3c929b83637
nanyamax <ifunanyasunday7@gmail.com>
2022-10-10 13:20:15 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - prints the name of the file
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
