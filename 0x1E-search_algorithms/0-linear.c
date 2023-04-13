6c8b0166e02cc7bfa0b6eac339b4055b0c784f7e
nanyamax <ifunanyasunday7@gmail.com>
2023-04-13 14:02:12 -0700

    binary

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "search_algos.h"
+
 /**
   * linear_search - Searches for a value in an array
   *                 of integers using linear search.
@@ -12,18 +12,18 @@
   *
   * Description: Prints a value every time it is compared in the array.
   */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
 
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
+	if (array == NULL)
+		return (-1);
 
-    for (size_t i = 0; i < size; i++) {
-        printf("Comparing %d to %d\n", array[i], value);
-        if (array[i] == value) {
-            return i;
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 
-    return -1;
-}
+	return (-1);
